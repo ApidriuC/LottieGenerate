02bfdb144a51bb6b77a45ae0ba6a965a7a2e21dc
Andrés Felipe Londoño Campos <78039311+ApidriuC@users.noreply.github.com>
2022-08-12 15:06:17 -0500

    Update Default Values

--- LottieComponent.js
+++ LottieComponent.js
@@ -1,62 +1,58 @@
-﻿function LottieComponent(_json) {
-    //Validación entrada Objeto o Json
-    if (typeof _json == 'object') {
-        _json = _json;
-    } else if (typeof _json == 'string') {
-        _json = JSON.parse(_json);
+class LottieClass {
+
+    constructor(_json) {
+        //Validación entrada tipo Objeto o JSON
+        if (typeof _json == 'object') {
+            _json = _json;
+        } else if (typeof _json == 'string') {
+            _json = JSON.parse(_json);
+        }
+        //Asignación de valores por entrada y Valores por defecto
+        this.Divid = _json.Divid?.toString() || 'newDivId';
+        this.Divclass = _json.Divclass?.toString() || 'content-NewDiv';
+        this.HostDiv = _json.HostDiv;
+        this.lottieId = _json.lottieId?.toString() || 'lottie-player';
+        this.lottieClass = _json.lottieClass?.toString() || 'newLottie-Player';
+        this.lottieSrc = _json.lottieSrc;
+        this.lottieSpeed = _json.lottieSpeed?.toString() || '1';
+        this.lottieLoop = _json.lottieLoop?.valueOf() || false;
+        this.lottieControls = _json.lottieControls?.valueOf() || false;
     }
-    //Asignación de varibales Div
-    var newDivId = _json.Divid;
-    var newDivClassName = _json.Divclass;
-    var hostDiv = _json.HostDiv;
-    var newDivWidth = _json.DivWidth;
-    var newDivHeight = _json.DivHeight;
-    //Creación del Contenedor Lottie
-    var newDiv = document.createElement('div');
-    newDiv.id = newDivId;
-    newDiv.className = newDivClassName;
-    //Agregación del Div al HostDiv
-    $(hostDiv).append(newDiv);
-    //Atributos css
-    $('#NewDivId').css({
-        "width": newDivWidth,
-        "height": newDivHeight,
-    });
 
-    //Asignación de varibales Lottie
-    var lottiePlayerId = _json.lottieId;
-    var lottiePlayerIdClassName = _json.lottieClass;
-    var lottiePlayerSrc = _json.lottieSrc;
-    var lottiePlayerSpeed = _json.lottieSpeed;
-    var lottiePlayerLoop = _json.lottieLoop;
-    var lottiePlayerAutoPlay = _json.lottieAutoPlay;
-    //var lottiePlayerAutoplay = _json.lottieAutoplay;
-    var lottiePlayerControls = _json.lottieControls;
-    //Creación del Lottie
-    var lottiePlayer = document.createElement('lottie-player');
-    lottiePlayer.id = lottiePlayerId;
-    lottiePlayer.className = lottiePlayerIdClassName;
-    //Valor por defecto para animación de entrada
-    //lottiePlayer.style.display = 'none';
-    //Agregación del Lottie al HostDiv
-    $('#NewDivId').append(lottiePlayer);
-    //Animación de entrada
-    //$('#NewLottie-Player').toggle("slow");
-    //Asignación de atributos
-    $('#NewLottie-Player').attr({
-        "src": lottiePlayerSrc, //Fuente tipo JSON
-        "speed": lottiePlayerSpeed, //Velocidad de la animación
-        "loop": lottiePlayerLoop, //Si/No Bucle de la animación
-        "autoplay": lottiePlayerAutoPlay, //Si/No Reproduce al iniciar la función
-        "controls": lottiePlayerControls, //Si/No controles de la animación
-        "background": "transparent" //Color de fondo de la animación
-    });
-    //Atributos css (Default)
-    $('#NewLottie-Player').css({
-        "width": "auto",
-        "height": "auto",
-    });
+    generarLottie() {
 
+        //Creación del Contenedor Lottie
+        var newDiv = document.createElement('div');
+        newDiv.id = this.Divid;
+        newDiv.className = this.Divclass;
+        //Agregación del Div al HostDiv
+        $(this.HostDiv).append(newDiv);
+        //Atributos css
+        $('#' + this.Divid).css({
+            "width": "100%",
+            "height": "100%",
+        });
+
+        //Creación del Lottie
+        var lottiePlayer = document.createElement('lottie-player');
+        lottiePlayer.id = this.lottieId;
+        lottiePlayer.className = this.lottieClass;
+        //Agregación del Lottie al HostDiv
+        $('#' + this.Divid).append(lottiePlayer);
+        //Asignación de atributos
+        $('#' + this.lottieId).attr({
+            "src": this.lottieSrc, //Fuente tipo JSON
+            "speed": this.lottieSpeed, //Velocidad de la animación
+            "loop": this.lottieLoop, //Si/No Bucle de la animación
+            "autoplay": true, //Si/No Reproduce al iniciar la función
+            "controls": this.lottieControls, //Si/No controles de la animación
+            "background": "transparent" //Color de fondo de la animación
+        });
+        //Atributos css (Default)
+        $('#' + this.lottieId).css({
+            "width": "100%",
+            "height": "100%",
+        });
+    }
 }
-
 
